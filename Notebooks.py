5dda349c85193e4affdb9a54f6ec61c161401421
jdetoro <91838515+jdetoro@users.noreply.github.com>
2021-12-13 08:33:39 +0100

    Update Notebooks.py

--- Notebooks.py
+++ Notebooks.py
@@ -1,30 +1,24 @@
 import json
-import os
 
-with open('C:/Users/Javier/Desktop/TFG/Programa/test(somef).json') as json_file:
-        data = json.load(json_file)
-#        print(data)
-        i = 0
+class Notebooks:
+    def Note(self):
+        with open('C:/Users/Javier/Desktop/TFG/Programa/ProyectoUnificado/NewPythonProject/src/test(somef).json') as json_file:
+            data = json.load(json_file)
+            i = 0
 
-        for directory, files in data.items():
-#            print (directory)
-            if directory == "hasExecutableNotebook":
-                with open ('C:/Users/Javier/Desktop/TFG/Programa/README.md','a+') as h:
-                    h.write('## Notebooks\n')
-                    h.write('You can try the following examples in Binder:\n')
-                    for a,b in files.items():
-                        if a == "excerpt":
-                        
-                            #print ("##Notebooks\n")
-                            #print ("You can try the following examples in Binder:\n")
-                            for iter in b:
-                                i=i+1
-                              
-                                h.write (" "+ str(i) + ". Example:")
-                                h.write ("[![Binder](https://mybinder.org/badge_logo.svg)]("+iter+")\n")
-                         #   print(i,". Example:")
-                         #  print ("[![Binder](https://mybinder.org/badge_logo.svg)](",iter,")\n")
-                    #file.write(a+"\n")
-                   
-               
-        
+            for directory, files in data.items():
+                if directory == "hasExecutableNotebook":
+                    with open ('C:/Users/Javier/Desktop/TFG/Programa/ProyectoUnificado/NewPythonProject/src/README.md','a+') as h:
+                        h.write('## Notebooks\n')
+                        h.write('You can try the following examples in Binder:\n')
+                        for a,b in files.items():
+                            if a == "excerpt":
+                                for iter in b:
+                                    i=i+1
+                                    #print(iter)
+                                    cadena = iter.split("/")
+                                    nombre = cadena.pop()
+                                    #print(nombre)
+                                    h.write (" "+ str(i) + ". "+ nombre +": ")
+                                    h.write ("[![Binder](https://mybinder.org/badge_logo.svg)]("+iter+")\n")
+    pass
