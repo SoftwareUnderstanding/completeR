99d122d641c3f9b609f82a424b9d7d2aa5e0e647
jdetoro <91838515+jdetoro@users.noreply.github.com>
2021-12-21 10:13:03 +0100

    Update Notebooks.py

--- Notebooks.py
+++ Notebooks.py
@@ -1,24 +1,27 @@
 import json
 
 class Notebooks:
-    def Note(self):
-        with open('C:/Users/Javier/Desktop/TFG/Programa/ProyectoUnificado/NewPythonProject/src/test(somef).json') as json_file:
+    def Note(self,direct_out):
+        with open('C:/Users/Javier/Desktop/TFG/Programa/ProyectoUnificado/NewPythonProject/src/SalidasAux/test(somef).json') as json_file:
             data = json.load(json_file)
             i = 0
 
             for directory, files in data.items():
                 if directory == "hasExecutableNotebook":
-                    with open ('C:/Users/Javier/Desktop/TFG/Programa/ProyectoUnificado/NewPythonProject/src/README.md','a+') as h:
+                    with open (direct_out+'/README.md','a+') as h:
                         h.write('## Notebooks\n')
                         h.write('You can try the following examples in Binder:\n')
+                        h.write('| Name  | Binder |\n')
+                        h.write('| ------------- | ------------- |\n')
                         for a,b in files.items():
                             if a == "excerpt":
                                 for iter in b:
                                     i=i+1
-                                    #print(iter)
                                     cadena = iter.split("/")
                                     nombre = cadena.pop()
-                                    #print(nombre)
-                                    h.write (" "+ str(i) + ". "+ nombre +": ")
-                                    h.write ("[![Binder](https://mybinder.org/badge_logo.svg)]("+iter+")\n")
+                                    #h.write ('| '+ nombre +' | [![Binder](https://mybinder.org/badge_logo.svg)]|'\n) 
+                                    h.write ('| '+ nombre +' | ![Binder](https://mybinder.org/badge_logo.svg)|\n')
+                                    #h.write (" "+ str(i) + ". "+ nombre +": " )
+                                    #h.write ("[![Binder](https://mybinder.org/badge_logo.svg)]("+iter+")\n")
+                        h.write('\n')
     pass
