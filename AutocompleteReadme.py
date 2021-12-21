84eb0cb0043f968d9a70f7f94c2c970f0203bccb
jdetoro <91838515+jdetoro@users.noreply.github.com>
2021-12-21 10:06:04 +0100

    Update AutocompleteReadme.py

--- AutocompleteReadme.py
+++ AutocompleteReadme.py
@@ -5,7 +5,7 @@ import MissingFields
 import Notebooks
 import DockerImage
 import Software
-
+import shutil
 
 a = ControlDocumentation.ControlDocumentation()
 b = ExportRequirements.ExportRequirements()
@@ -15,10 +15,27 @@ e = Notebooks.Notebooks()
 f = DockerImage.DockerImage()
 g = Software.Software()
 
-a.control()
-b.exportReq()
-c.License()
-d.Missing()
-e.Note()
-f.Docker()
-g.Software()
+
+
+import click
+@click.command()
+@click.option('--direct_in','-i', type=str,required=True,help="Theres the path of the directory to inspect")
+@click.option('--direct_out','-o', type=str,required=True,help="Theres the path of the directory save auxiliary documents(Requiremets.txt, Missing.md, Readme.md (modified)")
+@click.option('--replace_readme','-r', type=bool, is_flag=True, default=False,help="Replace the README file of the repository")
+
+def main(direct_in,direct_out,replace_readme):
+    shutil.copy(direct_in+"/README.md", direct_out+"/README.md")
+    a.control(direct_out)
+    b.exportReq(direct_in,direct_out)
+    c.License(direct_out)
+    d.Missing(direct_out)
+    e.Note(direct_out)
+    f.Docker(direct_in,direct_out)
+    g.Software(direct_out)
+    if replace_readme:
+        print("Si se activa")
+        #os.remove(direct_in+"/README.md")
+        shutil.copy(direct_out+"/README.md", direct_in+"/README.md")
+
+if __name__ == "__main__":
+    main()
