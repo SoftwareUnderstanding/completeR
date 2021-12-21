1afa6c31e615ce63a1b469721be591956f096986
jdetoro <91838515+jdetoro@users.noreply.github.com>
2021-12-21 10:09:41 +0100

    Update License.py

--- License.py
+++ License.py
@@ -1,12 +1,12 @@
 import json
 
 class License:
-    def License(self):
-        with open('C:/Users/Javier/Desktop/TFG/Programa/ProyectoUnificado/NewPythonProject/src/test(somef).json') as json_file:
+    def License(self,direct_out):
+        with open('C:/Users/Javier/Desktop/TFG/Programa/ProyectoUnificado/NewPythonProject/src/SalidasAux/test(somef).json') as json_file:
             data = json.load(json_file)
             for directory, files in data.items():
                 if directory == "license":
-                    with open ('C:/Users/Javier/Desktop/TFG/Programa/ProyectoUnificado/NewPythonProject/src/README.md','a+') as h:
+                    with open (direct_out+'/README.md','a+') as h:
                         h.write('## License\n')
 #                       h.write('You can try the following examples in Binder:\n')
                         for a,b in files.items():
